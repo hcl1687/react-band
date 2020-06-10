266ffa38a80b92eaeca10ea5099040ee4f3e958c
Chunlin He <hechunlin@nd.com.cn>
2020-06-10 12:10:48 +0800

    adjust test folder

--- jest.config.js
+++ jest.config.js
@@ -67,8 +67,8 @@ module.exports = {
 
   // A map from regular expressions to module names that allow to stub out resources with a single module
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/test/__mocks__/fileMock.js',
-    '\\.(css|less)$': '<rootDir>/test/__mocks__/styleMock.js'
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/tests/__mocks__/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/tests/__mocks__/styleMock.js'
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
