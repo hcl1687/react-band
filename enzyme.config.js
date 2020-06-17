8c4e1df003e17eb3d64c907b59c622f7efea899d
Chunlin He <hechunlin@nd.com.cn>
2020-06-17 18:30:08 +0800

    unit test: fix-the-warning-uselayouteffect-does-nothing-on-the-server

--- enzyme.config.js
+++ enzyme.config.js
@@ -1,4 +1,7 @@
 import Adapter from 'enzyme-adapter-react-16'
+import React from 'react'
 import { configure } from 'enzyme'
+// https://stackoverflow.com/questions/58070996/how-to-fix-the-warning-uselayouteffect-does-nothing-on-the-server
+React.useLayoutEffect = React.useEffect
 
 configure({ adapter: new Adapter() })
