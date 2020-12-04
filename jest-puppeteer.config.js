c01632789a82878e39e817aaaab4a4911353c049
Chunlin He <hechunlin@nd.com.cn>
2020-12-04 14:51:59 +0800

    remove puppeteer

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,4 +1,14 @@
+  
+require('module-alias/register')
+const chromePaths = require('chrome-paths')
+
 module.exports = {
+  launch: {
+    headless: true,
+    slowMo: 250,
+    timeout: 0,
+    executablePath: chromePaths.chrome,
+  },
   server: {
     command: 'npm run start'
   }
