5c3a262c4be318590b78ef9f07437adee4040c00
Colin He <hcl1687@gmail.com>
2021-01-16 21:53:58 +0800

    remove test files before build

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -3,7 +3,7 @@ const TerserPlugin = require('terser-webpack-plugin')
 const baseConfig = require('./webpack.base.config')
 const merge = require('webpack-merge')
 
-const config = merge(baseConfig, {
+const config = merge(baseConfig('production'), {
   mode: 'production',
   optimization: {
     minimizer: [new TerserPlugin()]
