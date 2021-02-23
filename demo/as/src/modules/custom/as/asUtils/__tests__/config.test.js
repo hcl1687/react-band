96c055fed304a141012033248cb52288f3fb6506
Chunlin He <hechunlin@nd.com.cn>
2021-02-23 14:39:59 +0800

    remove useless files

--- demo/as/src/modules/custom/as/asUtils/__tests__/config.test.js
+++
@@ -1,9 +0,0 @@
-import configFactory from '../config'
-
-const config = configFactory()
-
-describe('custom/as/asUtils/config', () => {
-  it('should render correctly', () => {
-    expect(config).toMatchSnapshot()
-  })
-})
--- demo/as/src/modules/custom/as/components/multiView/__tests__/config.test.js
+++
@@ -1,9 +0,0 @@
-import configFactory from '../config'
-
-const config = configFactory()
-
-describe('custom/as/components/multiView/config', () => {
-  it('should render correctly', () => {
-    expect(config).toMatchSnapshot()
-  })
-})
