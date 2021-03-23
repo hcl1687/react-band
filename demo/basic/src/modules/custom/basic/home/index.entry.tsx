5da4235c4439c9d05458743771ec524e7a8339f0
Chunlin He <hechunlin@nd.com.cn>
2021-03-23 10:04:32 +0800

    update demo

--- demo/basic/src/modules/custom/basic/home/index.entry.tsx
+++ demo/basic/src/modules/custom/basic/home/index.entry.tsx
@@ -1,7 +1,11 @@
 import PropTypes, { InferProps } from 'prop-types'
 import React from 'react'
+import darkgray from './themes/darkgray/index.css'
+import defaultTheme from './themes/default/index.css'
+import en from './i18n/en.json'
+import zhCN from './i18n/zh-CN.json'
 
-export default (): RB.IRBComponent => {
+const entry = (): RB.IRBComponent => {
   function Home (props: InferProps<typeof Home.propTypes>) {
     const handleClick = () => {
       const history = props.history as RB.IRBHistory
@@ -23,4 +27,30 @@ export default (): RB.IRBComponent => {
   }
 
   return Home
+}
+
+const i18n = (RB_CONTEXT: RB.IRBContext): RB.IRBI18n => {
+  const { locale } = RB_CONTEXT.options
+  const i18ns = {
+    en,
+    'zh-CN': zhCN
+  }
+
+  return i18ns[locale]
+}
+
+const theme = (RB_CONTEXT: RB.IRBContext): RB.IRBTheme => {
+  const { theme } = RB_CONTEXT.options
+  const themes = {
+    default: defaultTheme,
+    darkgray
+  }
+
+  return themes[theme] || defaultTheme
+}
+
+export default {
+  entry,
+  i18n,
+  theme
 }
